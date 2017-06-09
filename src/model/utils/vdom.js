c93228b5065226b8f70db3e0ceb5ded19665d28d
Yang Xingqiu <196948296@qq.com>
2017-06-09 15:59:13 +0800

    重构

--- src/model/utils/vdom.js
+++
@@ -1,9 +0,0 @@
-import { hasOwn } from '@/components/public/tree/model//utils/util';
-
-export function isVNode(node) {
-  return typeof node === 'object' && hasOwn(node, 'componentOptions');
-};
-
-export function getFirstComponentChild(children) {
-  return children && children.filter(c => c && c.tag)[0];
-};
